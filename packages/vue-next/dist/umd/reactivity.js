b9ed0f63e6094a52b61227f159ae85c79d199922
kira0524 <kiroader@gmail.com>
2020-12-07 16:45:12 +0800

    fix: update

--- packages/vue-next/dist/umd/reactivity.js
+++
@@ -1,9 +0,0 @@
-(function (factory) {
-	typeof define === 'function' && define.amd ? define(factory) :
-	factory();
-}((function () { 'use strict';
-
-	console.log('000');
-
-})));
-//# sourceMappingURL=reactivity.js.map
