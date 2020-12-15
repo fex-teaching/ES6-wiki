15c3f6a3539babfb451cd81f1c9a9e9fa2c8e35d
kira0524 <kiroader@gmail.com>
2020-12-15 19:56:37 +0800

    feat: update

--- packages/vue-next/src/reactivity-1/baseHandlers.js
+++ packages/vue-next/src/reactivity-1/baseHandlers.js
@@ -1,17 +1,25 @@
+import { reactive } from './reactive';
+
 function createGetter() {
   return function get(target, key, receiver) {
-    console.log('获取值');
     const res = Reflect.get(target, key, receiver);
+    if (typeof key == "symbol") {
+      return res;
+    }
+    console.log("获取值，调用get方法"); // 拦截get方法
+    if (isObject(res)) {
+      return reactive(res);
+    }
     return res;
-  }
+  };
 }
 
 function createSetter() {
   return function get(target, key, value, receiver) {
-    console.log('设置值');
+    console.log("设置值");
     const res = Reflect.set(target, key, value, receiver);
     return res;
-  }
+  };
 }
 
 function deleteProperty(target, key) {
@@ -19,8 +27,8 @@ function deleteProperty(target, key) {
   return res;
 }
 
-const get = createGetter()
-const set = createSetter()
+const get = createGetter();
+const set = createSetter();
 
 export const mutableHandlers = {
   get,
@@ -28,5 +36,4 @@ export const mutableHandlers = {
   deleteProperty,
   // has,
   // ownKeys
-}
-
+};
